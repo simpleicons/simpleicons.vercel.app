4a02bfa5206aaf10121bc81633de62997db313ae
Andre_601 <github@andre601.ch>
2022-03-11 20:50:48 +0100

    Try out size option

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const icons = require('./icons.js')
 const serveHomepage = require('./home.js')
 
 const serveIcon = (req, res, params) => {
-  const { name, color } = params
+  const { name, color, size } = params
 
   if (icons[name]) {
     res.setHeader('Content-Type', 'image/svg+xml')
     res.setHeader('Cache-Control', 'public, max-age=86400, s-maxage=31536000, stale-while-revalidate=604800')
-    return send(res, 200, icons[name].toSVG({ fill: `#${color}`, width: '', height: '', xmlns: 'http://www.w3.org/2000/svg'}))
+    
+    if (color === '') {
+      color = '000'
+    }
+    
+    return send(res, 200, icons[name].toSVG({ fill: `#${color}`, width: `${size}`, height: `${size}`, xmlns: 'http://www.w3.org/2000/svg'}))
   }
 
   send(res, 404)
