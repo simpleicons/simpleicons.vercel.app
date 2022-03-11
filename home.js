523474c453910d7f9447b72267fa93c51ef2de57
Andre_601 <github@andre601.ch>
2022-03-11 01:32:37 +0100

    Update home.js

--- home.js
+++ home.js
@@ -28,7 +28,7 @@ const serveHome = serveMarked(
       
       <link rel="manifest" href="https://www.andre601.ch/assets/site.webmanifest">
       
-      <link href="/stylesheets/style.css" rel="stylesheet">
+      <link href="/assets/stylesheets/style.css" rel="stylesheet">
     `,
     beforeBodyEnd: `
       <div id="icons" class="markdown-body">
